e3ae528dcbacfd9f143c5cce361202e84d1affd3
Luke Storey <lukemstorey@gmail.com>
2022-04-26 15:40:52 +0100

    Refactor to use context blocks

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,17 +2,14 @@ describe Oystercard do
   max_balance = Oystercard::MAX_BALANCE
   min_charge = Oystercard::MIN_CHARGE
   fare = min_charge
-
-  def top_up_card
-    subject.top_up(20)
-  end
-
+  
   describe '#balance' do
     it 'should have default value of 0' do
       expect(subject.balance).to eq(0)
     end
   end
 
+  
   describe '#top_up' do
     it 'should increase the balance by the amount' do
       amount = 20
@@ -25,29 +22,32 @@ describe Oystercard do
     end
   end
 
-  describe '#touch_in' do
-    it 'should change card status to be in use' do
-      top_up_card
-      subject.touch_in
-      expect(subject).to be_in_journey
+  context 'card balance is topped up' do
+    before(:each) { subject.top_up(max_balance) }
+    
+    describe '#touch_in' do
+      it 'should change card status to be in use' do
+        subject.touch_in
+        expect(subject).to be_in_journey
+      end
+
+      it 'should not let the user touch in with a balance below the minimum' do
+        empty_card = Oystercard.new
+        expect{ empty_card.touch_in }.to raise_error "Insufficient funds - balance below #{min_charge}"
+      end
+
     end
 
-    it 'should not let the user touch in with a balance below the minimum' do
-      expect{ subject.touch_in }.to raise_error "Insufficient funds - balance below #{min_charge}"
-    end
+    describe '#touch_out' do
+      it 'should change card status to not be in use' do
+        subject.touch_in
+        subject.touch_out(fare)
+        expect(subject).to_not be_in_journey
+      end
 
-  end
-
-  describe '#touch_out' do
-    it 'should change card status to not be in use' do
-      top_up_card
-      subject.touch_in
-      subject.touch_out(fare)
-      expect(subject).to_not be_in_journey
-    end
-
-    it 'should deduct the fare from the balance' do 
-      expect{ subject.touch_out(fare) }.to change { subject.balance }.by(-fare) 
+      it 'should deduct the fare from the balance' do 
+        expect{ subject.touch_out(fare) }.to change { subject.balance }.by(-fare) 
+      end
     end
   end
 end
